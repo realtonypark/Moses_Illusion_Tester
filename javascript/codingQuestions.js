a26a0a7d843e469e8c895a4899b16d38c9b392ca
realtonypark <34884758+realtonypark@users.noreply.github.com>
2022-11-26 22:20:49 +0900

    Question Update

--- javascript/codingQuestions.js
+++ javascript/codingQuestions.js
@@ -428,32 +428,32 @@ var questions = [
     answer: "False"
   },
   {
-    title: "5. It was 세종 that made new Korean character 한글",
+    title: "5. It was 세종 that made new Korean character 한글.",
     choices: ["True", "False"],
     answer: "True"
   },
   {
-    title: "6. It is a man that is legally allowed to marry his widow’s sister",
+    title: "6. It is a man that is legally allowed to marry his widow’s sister.",
     choices: ["True", "False"],
     answer: "False"
   },
   {
-    title: "7. It was by seven dwarfs that Cinderella was sheltered before marrying her prince",
-    choices: ["True", "False"],
-    answer: "False"
-  },
-  {
-    title: "8. It was United Kingdom that Margaret Thatcher was a prime minister of",
+    title: "7. It is circle whose area is pi-r-squared.",
     choices: ["True", "False"],
     answer: "True"
   },
   {
-    title: "9. It was the prince that found the glass slipper left at the ball by Snow White",
+    title: "8. It was United Kingdom that Margaret Thatcher was a president of.",
     choices: ["True", "False"],
     answer: "False"
   },
   {
-    title: "10. It is on Halloween that children go door to door, dressed in costume, giving out candy",
+    title: "9. It was the prince that found the glass slipper left at the hall by Snow White.",
+    choices: ["True", "False"],
+    answer: "False"
+  },
+  {
+    title: "10. It is on Halloween that children go door to door, dressed in costume, giving out candy.",
     choices: ["True", "False"],
     answer: "False"
   },
@@ -463,12 +463,12 @@ var questions = [
     answer: "False"
   },
   {
-    title: "12. It is circle whose area is pi-r-squared",
+    title: "12.It was by seven dwarfs that Cinderella was sheltered before marrying her prince.",
     choices: ["True", "False"],
-    answer: "True"
+    answer: "False"
   },
   {
-    title: "13. It is Jack-O-Lantern that is a carved pumpkin displayed on Halloween",
+    title: "13. It is Jack-O-Lantern that is a carved pumpkin displayed on Halloween.",
     choices: ["True", "False"],
     answer: "True"
   },
@@ -478,112 +478,112 @@ var questions = [
     answer: "False"
   },
   {
-    title: "15. It is Santa that is n in the red suit and long white beard who gives out birthday presents from his sleigh",
+    title: "15. It is Santa that is n in the red suit and long white beard who gives out birthday presents from his sleigh.",
     choices: ["True", "False"],
     answer: "False"
   },
   {
-    title: "16. It was gravity that by having an apple fall on his head, Newton discover",
+    title: "16. It was gravity that by having an apple fall on his head, Newton discover.",
     choices: ["True", "False"],
     answer: "True"
   },
   {
-    title: "17. It is an orphanage where children stay when they are aborted",
-    choices: ["True", "False"],
-    answer: "False"
-  },
-  {
-    title: "18.It was Neil Armstrong who was the first man to walk on the school",
-    choices: ["True", "False"],
-    answer: "False"
-  },
-  {
-    title: "19. It is 26 numbers that are there in the alphabet",
-    choices: ["True", "False"],
-    answer: "False"
-  },
-  {
-    title: "20. It was electricity that by flying a kite, discovered by Franklin",
+    title: "17. It is an orphanage where children stay when they are aborted.",
     choices: ["True", "False"],
     answer: "True"
   },
   {
-    title: "21. It was Yi Sunshin that was shot in World War II",
+    title: "18.It was Neil Armstrong who was the first man to walk on the school.",
     choices: ["True", "False"],
     answer: "False"
   },
   {
-    title: "22. It was air crash that made survivors buried",
+    title: "19. It is an alphabet that has 26 numbers.",
     choices: ["True", "False"],
     answer: "False"
   },
   {
-    title: "23. It was the United States that the inventor of the telephone, Alexander Graham Bell had nationality of",
+    title: "20. It was electricity that by flying a kite, discovered by Franklin.",
     choices: ["True", "False"],
     answer: "True"
   },
   {
-    title: "24. It is water that contains two atoms of metal ions and one oxygen",
+    title: "21. It was 이순신 that was shot in World War II.",
     choices: ["True", "False"],
     answer: "False"
   },
   {
-    title: "25. It is in 365 days that the earth circles around the sun",
-    choices: ["True", "False"],
-    answer: "True"
-  },
-  {
-    title: "26. It is from the United Kingdom that J. K. Rowling, the author of the well-known Harry Potter, originates",
-    choices: ["True", "False"],
-    answer: "True"
-  },
-  {
-    title: "27. It was the bad queen that gave the poisoned apple to prince",
+    title: "22. It is air crash that makes survivors buried.",
     choices: ["True", "False"],
     answer: "False"
   },
   {
-    title: "28. It is white colour of the fur that the bears on the North Pole have",
+    title: "23. It was the United States that the inventor of the telephone, Alexander Graham Bell had nationality of.",
     choices: ["True", "False"],
     answer: "True"
   },
   {
-    title: "29. It was Mona Lisa that made the Italian painter Michelangelo famous",
+    title: "24. It is water that contains two atoms of metal ions and one oxygen.",
     choices: ["True", "False"],
     answer: "False"
   },
   {
-    title: "30. QuestionIt is in a straight line that while marching, ants often carry food back to their home.",
+    title: "25. It is in 365 days that the earth circles around the sun.",
     choices: ["True", "False"],
     answer: "True"
   },
   {
-    title: "31. When writing, you can use the eraser on the end of the pen to erase mistakes",
+    title: "26. It is from the United Kingdom that J. K. Rowling, the author of the well-known Harry Potter, originates.",
+    choices: ["True", "False"],
+    answer: "True"
+  },
+  {
+    title: "27. It was Napoleon that dominated Asia after the French Revolution.",
     choices: ["True", "False"],
     answer: "False"
   },
   {
-    title: "32. Many children like to spread peanut butter and grape juice on their bread.",
+    title: "28. It is white color of the fur that the bears on the North Pole have.",
+    choices: ["True", "False"],
+    answer: "True"
+  },
+  {
+    title: "29. It was Mona Lisa that made the Italian painter Michelangelo famous.",
     choices: ["True", "False"],
     answer: "False"
   },
   {
-    title: "33. When athletes win first place in the Olympics, they get a gold medal to wear",
+    title: "30. It is in a straight line that while marching, ants often carry food back to their home.",
     choices: ["True", "False"],
     answer: "True"
   },
   {
-    title: "34. Some kids dislike eating green vegetables like spinach, corn, and peas",
+    title: "31. It is when writing that you can use the eraser on the end of the pen to erase mistakes.",
     choices: ["True", "False"],
-    answer: "True"
+    answer: "False"
   },
   {
-    title: "35. Most people eat dinner while sitting on a chair in the kitchen.",
+    title: "32. It is many children that like to spread peanut butter and grape juice on their bread.",
     choices: ["True", "False"],
-    answer: "True"
+    answer: "False"
   },
   {
-    title: "36. Most people eat sweet things for dessert like ice cream, beer, and browni",
+    title: "33. It is in the Olympics that when athletes win first place, they get a plastic medal to wear.",
+    choices: ["True", "False"],
+    answer: "False"
+  },
+  {
+    title: "34.It is some kids that dislike eating green vegetables like spinach, corn, and peas.",
+    choices: ["True", "False"],
+    answer: "False"
+  },
+  {
+    title: "35.It is  dinner that most people eat while sitting on a refrigerator in the kitchen.",
+    choices: ["True", "False"],
+    answer: "False"
+  },
+  {
+    title: "36. It is sweet things that most people eat for dessert like bear, ice cream, and brown.",
     choices: ["True", "False"],
     answer: "False"
   },
